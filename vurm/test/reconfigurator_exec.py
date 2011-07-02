057416b018729af6a6ec0b09e4c2ffe6930d4e75
Jonathan Stoppani <jonathan@stoppani.name>
2011-07-01 18:02:31 -0600

    Corrected a little bug in the argument counting.

--- vurm/test/reconfigurator_exec.py
+++ vurm/test/reconfigurator_exec.py
@@ -5,7 +5,7 @@ Simple test script to be invoked as replacement for the scontrol binary.
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) < 1:
+    if len(sys.argv) < 2:
         sys.exit(1)
 
     if sys.argv[1] == 'succeed':
