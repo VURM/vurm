44fc531234f08efe0d891e8d94b687a18ff3fd57
Jonathan Stoppani <jonathan@stoppani.name>
2011-07-02 18:17:15 -0600

    Documented the errors module.

--- vurm/error.py
+++ vurm/error.py
@@ -9,19 +9,39 @@ from twisted.spread import pb
 
 
 class RemoteVurmException(pb.Error):
-    pass
+    """
+    A marker class to mark exceptions having this class as superclass as
+    expected on the server side. They will be transparently raised remotely and
+    not logged by the server.
+
+    The remote user can access the type and the message of these exceptions.
+
+    NOTE: The traceback is hidden for these types of exceptions too. It is
+          possible to activate traceback passing for **ALL** exception types
+          by setting the ``debug`` directive to ``True`` in the configuration
+          file.
+    """
 
 
 
 class InsufficientResourcesException(RemoteVurmException):
-    pass
+    """
+    Raised when the current provisioners fail to fullfill a request for a given
+    operation due to a lack of resources.
+    """
 
 
 
 class ReconfigurationError(RemoteVurmException):
-    pass
+    """
+    Raised when the controller is not able to correctly reconfigure the SLURM
+    controller.
+    """
 
 
 
-class InvalidClusterName(pb.Error):
-    pass
+class InvalidClusterName(RemoteVurmException):
+    """
+    Raised when an operation which requires a cluster name fails because the
+    given cluster name is either invalid or was not found.
+    """
