b3a6dbf38e1355580046acef5e89791070a73488
Jonathan Stoppani <jonathan@stoppani.name>
2011-07-14 16:50:27 -0600

    Corrected a typo, removed unused imports and updated the __all__ variable.

--- vurm/commands.py
+++ vurm/commands.py
@@ -1,9 +1,7 @@
 from twisted.protocols import amp
 
-from lxml import etree
 
-
-__all__ = ['CreateDomain', 'DestroyDomain', 'SpawnSlurmDaemon', ]
+__all__ = ['CreateVirtualCluster', 'DestroyVirtualCluster', ]
 
 
 
@@ -13,12 +11,12 @@ class CreateVirtualCluster(amp.Command):
         ('minSize', amp.Integer(optional=True)),
     ]
     response = [
-        ('cluserName', amp.String()),
+        ('clusterName', amp.String()),
     ]
 
 
 
 class DestroyVirtualCluster(amp.Command):
     arguments = [
-        ('cluserName', amp.String()),
+        ('clusterName', amp.String()),
     ]
