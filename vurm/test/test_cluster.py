fa7b9ae9630269863c780fa02ce40d2efce9e180
Jonathan Stoppani <jonathan@stoppani.name>
2011-07-14 17:09:06 -0600

    Back to 100% test coverage for the cluster module

--- vurm/test/test_cluster.py
+++ vurm/test/test_cluster.py
@@ -10,18 +10,29 @@ from twisted.trial import unittest
 class SettingsTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.old_cluster_name_chars = cluster.CLUSTER_NAME_CHARS
+        self.old_cluster_name_length = cluster.CLUSTER_NAME_LENGTH
+        self.old_cluster_name_prefix = cluster.CLUSTER_NAME_PREFIX
+        self.old_node_name_prefix = cluster.NODE_NAME_PREFIX
 
 
     def tearDown(self):
-        pass
+        cluster.CLUSTER_NAME_CHARS = self.old_cluster_name_chars
+        cluster.CLUSTER_NAME_LENGTH = self.old_cluster_name_length
+        cluster.CLUSTER_NAME_PREFIX = self.old_cluster_name_prefix
+        cluster.NODE_NAME_PREFIX = self.old_node_name_prefix
+
+
+    def test_nodeNamesGenerator(self):
+        cluster.CLUSTER_NAME_PREFIX = ''
+        cluster.NODE_NAME_PREFIX = ''
+        names = cluster.VirtualCluster.nodeNamesGenerator('')
+        
+        for i in range(10):
+            self.assertEquals(i, int(next(names)[1:]))
 
 
     def test_nameIsUnique(self):
-        c = cluster.CLUSTER_NAME_CHARS
-        l = cluster.CLUSTER_NAME_LENGTH
-        p = cluster.CLUSTER_NAME_PREFIX
-
         cluster.CLUSTER_NAME_CHARS = 'ABC'
         cluster.CLUSTER_NAME_LENGTH = 2
         cluster.CLUSTER_NAME_PREFIX = ''
@@ -35,7 +46,6 @@ class SettingsTestCase(unittest.TestCase):
 
         name = names.pop()
         self.assertEquals(name, cluster.VirtualCluster.generateClusterName())
-
-        cluster.CLUSTER_NAME_CHARS = c
-        cluster.CLUSTER_NAME_LENGTH = l
-        cluster.CLUSTER_NAME_PREFIX = p
+        
+        name = names.pop()
+        self.assertEquals(name, cluster.VirtualCluster([]).name)
